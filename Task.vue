088d784bf0c5ee5136831534eb3a722aab9aa5a5
Rindusakinah <168341155+Rindusakinah@users.noreply.github.com>
2024-05-15 11:36:33 +0700

    Add files via upload

---
+++ Task.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <input type="checkbox" v-model="task.completed"> 
+      <span :class="{ 'completed': task.completed }">{{ task.title }}</span>
+      <button @click="editTask">Edit</button>
+      <button @click="deleteTask">Delete</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['task'],
+    methods: {
+      editTask() {
+        // Implementasi edit task
+      },
+      deleteTask() {
+        // Implementasi delete task
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .completed {
+    text-decoration: line-through;
+  }
+  </style>
---
+++ TaskForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <input type="text" v-model="title" placeholder="Enter task title">
+      <button @click="submitForm">{{ editing ? 'Update' : 'Add' }}</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: '',
+        editing: false
+      };
+    },
+    methods: {
+      submitForm() {
+        if (this.title.trim() === '') return;
+        if (this.editing) {
+          // Implementasi update task
+        } else {
+          // Implementasi add task
+        }
+        this.title = '';
+      }
+    }
+  }
+  </script>
+  
